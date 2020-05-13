0718c7356b5b4cacf220fd8e9b4e0365a8b6458d
Duke Ko <dukhyunko93@gmail.com>
2020-05-13 16:33:02 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -13,7 +13,11 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-
+  if(age>=13&&age<=19){
+  return 'You are a teenager!'
+} else if(age<=12){
+  return 'You are a kid'
+}
 }
 
 function ternaryTeenager(age) {
